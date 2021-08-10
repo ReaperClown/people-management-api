e6addec5a76fd3ffe3db1c499f37e97133d156ed
ReaperClown <the.willian.mesquita@gmail.com>
2021-08-09 21:00:26 -0300

    refactor: :rocket: Update controller class

---
+++ src/main/java/com/people/api/peopleapi/controller/PeopleController.java
@@ -0,0 +1,10 @@
+package com.people.api.peopleapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PeopleController {
+
+}
